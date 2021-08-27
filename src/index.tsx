d73fe055c03a75c1cf619b318bb62a07134186b0
Glooory <glooorypu@gmail.com>
2021-08-27 10:24:21 +0800

    Setup test app

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,4 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Fun Reacts</h1>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './public/index.html' }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  }
+}
